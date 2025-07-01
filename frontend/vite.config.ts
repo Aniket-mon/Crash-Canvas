13c4188a7c65e7ba019706a0d8543b780fcf04f4
Aniket Bhattacharya <aniket.bhattacharya1729@gmail.com>
2025-07-02 01:31:04 +0530

    deploy 2.0

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,12 +3,10 @@ import react from "@vitejs/plugin-react";
 import tailwind from "tailwindcss";
 import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-  ].filter(Boolean),
-  publicDir: "./public",
-  base: "/",
+export default defineConfig(() => ({
+  base: '/',
+  plugins: [react()],
+  publicDir: 'public',
   css: {
     postcss: {
       plugins: [tailwind()],
