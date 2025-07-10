6b4c5f7d3f06bc486515b8fa9174ba2a964afed9
Aniket Bhattacharya <aniket.bhattacharya1729@gmail.com>
2025-07-10 18:49:50 +0530

    fixes

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { StrictMode } from "react";
 import App from "./pages/App";
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
